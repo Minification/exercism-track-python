b1e06bcda8c454eafa12586f4b0ade03bcc20c67
Marius Hubatschek <marius.hubatschek@gmail.com>
2021-08-24 06:50:05 +0200

    Refactor bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,22 +1,18 @@
 import re
 
-def response(hey_bob):
-	hey_bob = re.sub(r"\s+", "", hey_bob)
-	if not hey_bob:
-		return "Fine. Be that way!"
-	all_caps = hey_bob == hey_bob.upper()
-	only_symbols_or_numbers = re.sub(r"[A-Za-z]", "", hey_bob) == hey_bob
-	is_question = hey_bob[-1] == "?"
-	has_fullstop = hey_bob[-1] == "." or re.compile(r"[A-Za-z0-9]").match(hey_bob[-1])
-	is_exclamation = hey_bob[-1] == "!"
-	if all_caps and not only_symbols_or_numbers:
-		if not is_question:
-			return "Whoa, chill out!"
-		else:
-			return "Calm down, I know what I'm doing!"
-	if is_question:
-		return "Sure."
-	if has_fullstop or is_exclamation:
-		return "Whatever."
-	return "End"
-	
+def is_true_upper(string: str) -> bool:
+    return string.lower() != string and string.upper() == string
+    
+def is_question(string: str) -> bool:
+    return string.strip().endswith("?")
+
+def response(hey_bob: str) -> str:
+	if is_true_upper(hey_bob) and is_question(hey_bob):
+	    return "Calm down, I know what I'm doing!"
+	elif is_true_upper(hey_bob):
+	    return "Whoa, chill out!"
+	elif is_question(hey_bob):
+	    return "Sure."
+	elif not hey_bob.strip():
+	    return "Fine. Be that way!"
+	return "Whatever."
